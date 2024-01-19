dfbc801f7aeab4805d210ac9fddc08c96a2602e9
Dadiga Vasavi Priya <112770492+dadigavasavipriya@users.noreply.github.com>
2024-01-19 08:23:04 +0530

    Update Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py

--- Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
+++ Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
@@ -1,4 +1,4 @@
-You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+'''You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
 
 Your task is to return an array where each object will have a new property 'greeting' with the following string value:
 
@@ -25,12 +25,13 @@ your function should return the following array:
   } 
 ]
 
+
+Notes:
+
+The order of the properties in the objects does not matter (except in COBOL).
+The input array will always be valid and formatted as in the example above.'''
 def greet_developers(lst): 
     # your code here
     for i in lst:
         i.update({'greeting':'Hi '+i['firstName']+', what do you like the most about '+i['language']+'?'})
     return lst
-Notes:
-
-The order of the properties in the objects does not matter (except in COBOL).
-The input array will always be valid and formatted as in the example above.
