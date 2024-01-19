46fe053f1b138e870ce0818c4b61cbe3bca466bf
Dadiga Vasavi Priya <112770492+dadigavasavipriya@users.noreply.github.com>
2024-01-19 10:21:08 +0530

    Create Coding Meetup #11 - Higher-Order Functions Series - Find the average age.py

---
+++ Coding Meetup #11 - Higher-Order Functions Series - Find the average age.py
@@ -0,0 +1,23 @@
+'''You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer.'''
+
+
+def get_average(lst): 
+    # your code here
+    list=[i['age'] for i in lst]
+    sum=0
+    for i in list:
+        sum+=i
+    return round(sum/len(list))
