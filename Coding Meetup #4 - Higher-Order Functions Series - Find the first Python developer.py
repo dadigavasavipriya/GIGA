5745ae8de5f8e5e3172abab01416c89a9a500ea4
Dadiga Vasavi Priya <112770492+dadigavasavipriya@users.noreply.github.com>
2024-01-19 08:27:40 +0530

    Update Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer.py

--- Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer.py
+++ Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer.py
@@ -1,4 +1,4 @@
-You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+'''You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
 
 Your task is to return one of the following strings:
 
@@ -15,4 +15,10 @@ your function should return Victoria, Puerto Rico.
 
 Notes:
 
-The input array will always be valid and formatted as in the example above.
+The input array will always be valid and formatted as in the example above.'''
+
+def get_first_python(users):
+    for i in users:
+        if i['language'] == "Python":
+            return i['first_name'] + ', ' + i['country']
+    return "There will be no Python developers"
