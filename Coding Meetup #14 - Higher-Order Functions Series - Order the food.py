272f7f9d9454569d3b523f9e15e2db6b64a33855
Dadiga Vasavi Priya <112770492+dadigavasavipriya@users.noreply.github.com>
2024-01-19 08:17:14 +0530

    Update and rename Gigacodewars.py to Coding Meetup #14 - Higher-Order Functions Series - Order the food.py

--- Coding Meetup #14 - Higher-Order Functions Series - Order the food.py
+++ Coding Meetup #14 - Higher-Order Functions Series - Order the food.py
@@ -40,29 +40,11 @@ def test_group():
         
         
 @test.describe('Random Tests')'''
-def order_food(lst): 
-    # your code here
-    d={}
-    meal={'vegetarian':0,'vegan':0,'diabetic':0,'standard':0,'gluten-intolerant':0}
+def order_food(lst):
+    d = {}
     for i in lst:
-        if i['meal']=='vegetarian':
-            a=meal['vegetarian']+1
-            meal.update({'vegetarian':a})
-            d.update({'vegetarian':a})
-        elif i['meal']=='vegan':
-            a=meal['vegan']+1
-            meal.update({'vegan':a})
-            d.update({'vegan':a})
-        elif i['meal']=='diabetic':
-            a=meal['diabetic']+1
-            meal.update({'diabetic':a})
-            d.update({'diabetic':a})
-        elif i['meal']=='standard':
-            a=meal['standard']+1
-            meal.update({'standard':a})
-            d.update({'standard':a})
+        if i['meal'] in d:
+            d[i['meal']] += 1
         else:
-            a=meal['gluten-intolerant']+1
-            meal.update({'gluten-intolerant':a})
-            d.update({'gluten-intolerant':a})
+            d[i['meal']] = 1
     return d
